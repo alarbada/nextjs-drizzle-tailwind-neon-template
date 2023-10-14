9d69159f52e59bec870c76824bb18643b9acbdf7
Guillem <you@example.com>
2023-10-14 13:03:18 +0200

    Add better layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,27 @@
 import './globals.css'
+import Link from 'next/link'
+
+import { Inter } from 'next/font/google'
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={inter.className}>
+      <body className="min-h-screen flex flex-col">
+        <div className="flex gap-4 p-4">
+          <Link className="hover:bg-slate-300 transition" href="/" prefetch={true}>
+            Home
+          </Link>
+          <Link className="hover:bg-slate-300 transition" href="/dashboard">
+            Dashboard
+          </Link>
+        </div>
+        <main className="flex flex-col items-center justify-top p-12">{children}</main>
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-top p-12 bg-neutral-950">
-      <h1 className="text-4xl font-bold text-white">Hello World</h1>
-    </main>
+    <h1 className="text-4xl font-bold text-white">Hello World</h1>
   )
 }
